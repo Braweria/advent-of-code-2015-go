92809490e650ba8ede58666bb0186fa0d695d539
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-11-28 11:41:50 +0100

    solve part 2 of day 3

--- days/03.go
+++ days/03.go
@@ -2,7 +2,6 @@ package days
 
 import (
 	"bufio"
-	"fmt"
 )
 
 type Coord struct {
@@ -13,6 +12,7 @@ type Visited map[Coord]int
 
 type Santa struct {
 	position     Coord
+	roboPosition Coord
 	visited      Visited
 	uniqueHouses int
 }
@@ -30,6 +30,19 @@ func (santa *Santa) moveTo(value string) {
 	}
 }
 
+func (santa *Santa) moveRoboTo(value string) {
+	switch value {
+	case "^":
+		santa.roboPosition.y++
+	case "v":
+		santa.roboPosition.y--
+	case "<":
+		santa.roboPosition.x--
+	case ">":
+		santa.roboPosition.x++
+	}
+}
+
 func (santa *Santa) deliver() {
 	visited, exists := santa.visited[santa.position]
 	if exists {
@@ -40,22 +53,35 @@ func (santa *Santa) deliver() {
 	}
 }
 
+func (santa *Santa) roboDeliver() {
+	visited, exists := santa.visited[santa.roboPosition]
+	if exists {
+		santa.visited[santa.roboPosition] = visited + 1
+	} else {
+		santa.visited[santa.roboPosition] = 1
+		santa.uniqueHouses++
+	}
+}
+
 func D03() {
 	file := GetFile("inputs/03.txt")
 	defer file.Close()
 
 	start := Coord{0, 0}
-	startVisited := Visited{start: 1}
-	santa := Santa{start, startVisited, 1}
+	startVisited := Visited{start: 2}
+	santa := Santa{start, start, startVisited, 1}
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanRunes)
 
-	for scanner.Scan() {
+	for i := 1; scanner.Scan(); i++ {
 		char := scanner.Text()
-		santa.moveTo(char)
-		santa.deliver()
+		if i%2 == 0 {
+			santa.moveRoboTo(char)
+			santa.roboDeliver()
+		} else {
+			santa.moveTo(char)
+			santa.deliver()
+		}
 	}
-
-	fmt.Println(santa.uniqueHouses)
 }
